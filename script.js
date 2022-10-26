e80ad9982ee698f4331e9c20b4e9fea49497b39b
eslam <eslam220077@gmail.com>
2022-10-26 19:44:42 +0300

    cw6 done

---
+++ script.js
@@ -0,0 +1,20 @@
+//جافا سكريبت ليست جافا
+
+alert("مرحبا بك في موقعي الذي سيحسب درجاتك !!");
+let grade = prompt("اكتب درجاتك بالأرقام");
+console.log(grade);
+if (grade >= 90 && grade <= 100) {
+  console.log("%c لقد حصلت على درجة امتياز", "color:#d62828");
+} else if (grade >= 90 && grade <= 100) {
+  console.log("%c لقد حصلت على درجة امتياز", "color:#d62828");
+} else if (grade >= 80 && grade <= 89) {
+  console.log("%c لقد حصلت على درجة امتياز", "color:#d62828");
+} else if (grade >= 70 && grade <= 79) {
+  console.log("%c لقد حصلت على درجة امتياز", "color:#d62828");
+} else if (grade >= 60 && grade <= 69) {
+  console.log("%c لقد حصلت على درجة امتياز", "color:#d62828");
+} else if (grade >= 50 && grade <= 59) {
+  console.log("%c لقد حصلت على درجة امتياز", "color:#d62828");
+} else {
+  console.log("%c لقد حصلت على درجة امتياز", "color:#d62828");
+}
